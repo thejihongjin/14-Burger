e0789c8aab54edf510c4088be565cd3e305305f1
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-05-30 10:04:43 -0700

    added initial files

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,12 @@
--- 2. In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
---    * Create the `burgers_db`.
---    * Switch to or use the `burgers_db`.
---    * Create a `burgers` table with these fields:
---      * **id**: an auto incrementing int that serves as the primary key.
---      * **burger_name**: a string.
---      * **devoured**: a boolean.
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN default false,
+    PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
 -- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name)
+VALUES (""),
+(""),
+("");
