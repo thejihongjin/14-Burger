958e5c6ef7dfade1f9401b0097fefafea5337297
unknown <1469406888@mil>
2019-05-31 16:20:46 -0700

    required express npm package inside server.js file

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 // 7. Require the following npm packages inside of the server.js file:
 //    * express
+
+var express = require("express");
+
+var PORT = process.env.PORT || 3306;
+
+var app = express();
+app.use(express.static("public")); // serve static content for the app from the "public" directory in the application directory
+app.use(express.urlencoded({ extended: true })); // parse application body as JSON
+app.use(express.json());
+
+var exphbs = require("express-handlebars"); // set Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js"); // import routes and give the server access to them
+app.use(routes);
+
+app.listen(PORT, function() { // start our server so that it can begin listening to client requests
+  console.log("Server listening on: http://localhost:" + PORT); // log (server-side) when our server has started
+});
