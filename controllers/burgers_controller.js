b8797cc660d6b956fdcc31fec775bbd37a987ecb
unknown <1469406888@mil>
2019-05-31 16:16:52 -0700

    imported Express and burger.js, created and exported the router for the app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,37 @@
-// 3. Inside the `burgers_controller.js` file, import the following:
-//    * Express
-//    * `burger.js`
-// 4. Create the `router` for the app, and export the `router` at the end of your file.
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(["name"], [req.body.name], function(result) {
+    res.json({ id: result.insertId }); // send back the ID of the new burger
+  });
+});
+
+router.put("/api/burger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end(); // if no rows were changed, then the ID must not exist, so 404
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
