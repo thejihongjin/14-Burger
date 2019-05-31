b499fbaceb5b76a6deac3a7548444e08e4f31fe1
unknown <1469406888@mil>
2019-05-31 15:49:57 -0700

    added 3 burger entries to seed.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
--- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
-
 INSERT INTO burgers (burger_name)
-VALUES (""),
-(""),
-("");
+VALUES ("hamburger"),
+("cheeseburger"),
+("grilled cheese");
