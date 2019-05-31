a0f03a0236a84ef7f40e47bb3e7c6047db3e93f5
unknown <1469406888@mil>
2019-05-31 16:11:00 -0700

    renamed burgers.js to burger.js, imported orm.js, and created and exported code to call ORM functions

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  
+  insertOne: function(cols, vals, cb) { // cols and vals variables are arrays
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
